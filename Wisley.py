bd74dc7a0a11adecc494e531488722e48c1ca576
MADDOX WISLEY <99274475+MaddoxW-11@users.noreply.github.com>
2022-02-11 15:59:11 +0000

    Put in .gitignore

--- Wisley.py
+++ Wisley.py
@@ -8,7 +8,7 @@
 
 team_name = 'E1'
 strategy_name = 'Collude'
-strategy_description = 'Always collude.'
+strategy_description = 'Always collude, no snitchy.'
     
 def move(my_history, their_history, my_score, their_score):
     '''Make my move based on the history with this player.
