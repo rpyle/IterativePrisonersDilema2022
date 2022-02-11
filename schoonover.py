1cf4d503df91d736bcd29e22eba0df67db4a0eec
AMANDA SCHOONOVER <99274306+amandaschoon@users.noreply.github.com>
2022-02-11 23:03:40 +0000

    added module Schoonover

--- schoonover.py
+++ schoonover.py
@@ -7,8 +7,8 @@
 ####
 
 team_name = 'schoonover'
-strategy_name = 'Collude'
-strategy_description = 'Always collude never betray.'
+strategy_name = 'copycat'
+strategy_description = 'collude first round. If you and your opponent answered the same on the previous round, you will keep your answer the same. If you and your opponent answered differently in the previous round, you will use their answer from the previous round.'
     
 def move(my_history, their_history, my_score, their_score):
     '''Make my move based on the history with this player.
@@ -20,6 +20,11 @@ def move(my_history, their_history, my_score, their_score):
     
     Returns 'c' or 'b' for collude or betray.
     '''
-    
-    # This player always colludes.
-    return 'c'
+    if len(my_history)==0: # It's the first round; collude.
+        return 'c'
+    elif my_history[-1] == their_history[-1]:
+        return my_history[-1]
+    elif my_history[-1] == "c" and their_history[-1] == "b":
+      return their_history[-1]
+    elif my_history[-1] == "b" and their_history[-1] == "c":
+      return their_history[-1]
