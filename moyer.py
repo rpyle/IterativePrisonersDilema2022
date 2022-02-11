e1f0d899d67331c0b2369ea774b515c7cf6803ac
RopTamen <99274437+344546@users.noreply.github.com>
2022-02-11 20:00:09 +0000

    Removed some comments

--- moyer.py
+++ moyer.py
@@ -8,23 +8,22 @@
 
 team_name = 'moyer'
 strategy_name = 'Malice'
-strategy_description = 'Always collude.'
+strategy_description = 'Collude but retaliate.'
   
 import random
 
 def move(my_history, their_history, my_score, their_score):
-    '''Make my move based on the history with this player.
     
-    history: a string with one letter (c or b) per round that has been played with this opponent.
-    their_history: a string of the same length as history, possibly empty. 
-    The first round between these two players is my_history[0] and their_history[0]
-    The most recent round is my_history[-1] and their_history[-1]
-    
-    Returns 'c' or 'b' for collude or betray.
-    '''
     if 'b' in their_history[-20:]:
       return 'b'
     else:
       return 'c'
     
-    
+    '''if len(my_history) == 0:
+      return 'c'
+    else:
+      if their_history[-1] == 'b':
+        return 'b'
+      elif their_history[-1] == 'c':
+        return 'c'''
+      
