b2bb7f5f80d837c997c3bfaa95865bbb1377d123
344220 <99274400+adriana-farace@users.noreply.github.com>
2022-02-16 16:55:42 +0000

    new code

--- farace.py
+++ farace.py
@@ -1,26 +1,21 @@
-####
-# Each team's file must define four tokens:
-#     team_name: a string
-#     strategy_name: a string
-#     strategy_description: a string
-#     move: A function that returns 'c' or 'b'
-####
+
 
 team_name = 'adriana'
-strategy_name = 'Collude'
-strategy_description = 'dont snitch.'
+strategy_name = 'come out on top'
+strategy_description = '''\
+Check the last 3 moves and betray if I've been betrayed in any of those past 3 moves. Otherwise, collude 97% of the time. This stategy allows me to betray only if I have been betrayed and give them the oppenent mostly the benefit of the doubt.
+'''
+
+import random
     
 def move(my_history, their_history, my_score, their_score):
-    '''Make my move based on the history with this player.
-    
-    history: a string with one letter (c or b) per round that has been played with this opponent.
-    their_history: a string of the same length as history, possibly empty. 
-    The first round between these two players is my_history[0] and their_history[0]
-    The most recent round is my_history[-1] and their_history[-1]
-    
-    Returns 'c' or 'b' for collude or betray.
-    '''
-    
-    # This player always colludes.
-    return 'c'
+  
+
+  if 'b' in their_history[-3:]: # If the other player has betrayed within last 3 rounds, 
+   return 'b'               # Betray.
+  else:
+    if random.random()<0.3: # 3% of the other rounds
+     return 'b'         # Betray
+    else:
+      return 'c'         # but 97% of the time collude
     
